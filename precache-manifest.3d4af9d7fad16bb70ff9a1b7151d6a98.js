f266a4b712ea3a0064110af26f35c62f3c7fb4cd
Susmit Japhalekar <susmit89@Susmits-MacBook-Pro-2.local>
2020-04-12 15:26:00 -0700

    Updates

---
+++ precache-manifest.3d4af9d7fad16bb70ff9a1b7151d6a98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55d7d88d9c8a7a9fc989f6eb97788311",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de7942c360d8df9bca1c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a17462e0edc71ba72b32",
+    "url": "/static/js/2.64dc598f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.64dc598f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de7942c360d8df9bca1c",
+    "url": "/static/js/main.281d2a96.chunk.js"
+  },
+  {
+    "revision": "91df99a66f13d700186e",
+    "url": "/static/js/runtime-main.c53c942f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
